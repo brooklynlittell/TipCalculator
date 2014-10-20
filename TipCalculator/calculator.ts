eb280f16896770d4914ad8eb68948ccdc98ad3f3
Brandon Littell <brandonl3737@gmail.com>
2014-10-20 18:26:40 -0400

    Added calculator files.

---
+++ TipCalculator/calculator.ts
@@ -0,0 +1,43 @@
+/// <reference path="jquery.d.ts"/>
+
+var billAmount: number;
+var tipPercent: number;
+var tipAmount: number;
+var totalAmount: number;
+
+function calculate()
+{
+    // Load info
+    this.loadInput();
+
+    if ( this.billAmount && this.tipPercent )
+    {
+        // Calculate totals and print values if amounts are valid
+        this.calculateTipTotal();
+
+        $("#tipAmountText").val(this.tipAmount.toFixed(2).toString());
+        $("#totalText").val(this.totalAmount.toFixed(2).toString());
+    }
+    else
+    {
+        // Nothing or NaN results
+        $("#tipAmountText").val("--");
+        $("#totalText").val("--");
+    }
+}
+
+function loadInput()
+{
+    // Load the bill amount
+    this.billAmount = parseFloat( $("#billAmountText").val() );
+
+    // Load the tip %
+    this.tipPercent = parseFloat( $("#tipPercentText").val() );
+}
+
+function calculateTipTotal()
+{
+    this.tipAmount = this.billAmount * (0.01 * this.tipPercent);
+    this.totalAmount = this.billAmount + this.tipAmount;
+}
+
